c6ab5ae60dc5197a647474933e842c44ec307f10
staimov <staimov@mail.ru>
2023-09-12 18:34:56 +0500

    dbconstants class added

---
+++ src/main/java/org/staimov/config/DBConstants.java
@@ -0,0 +1,12 @@
+package org.staimov.config;
+
+public class DBConstants {
+    public static final String DB_DIALECT = "org.hibernate.dialect.MySQL8Dialect";
+    public static final String DB_USER = "root";
+    public static final String DB_PASS = "root";
+    public static final String DB_HBM2DDL_AUTO = "validate";
+    public static final String DB_DRIVER = "com.p6spy.engine.spy.P6SpyDriver";
+    public static final String DB_URL = "jdbc:p6spy:mysql://localhost:63306/world";
+    public static final String CURRENT_SESSION_CONTEXT_CLASS = "thread";
+    public static final String STATEMENT_BATCH_SIZE = "100";
+}
