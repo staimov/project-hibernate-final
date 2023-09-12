eef4995d56d66d331988f219fe26e59c6137b47c
staimov <staimov@mail.ru>
2023-09-12 18:34:21 +0500

    dao classes added

--- src/main/java/org/staimov/dao/CityDAO.java
+++ src/main/java/org/staimov/dao/CityDAO.java
@@ -1,4 +1,30 @@
 package org.staimov.dao;
 
+import org.hibernate.SessionFactory;
+import org.hibernate.query.Query;
+import org.staimov.domain.City;
+
+import java.util.List;
+
 public class CityDAO {
+
+    private final SessionFactory sessionFactory;
+
+    public CityDAO(SessionFactory sessionFactory) {
+        this.sessionFactory = sessionFactory;
+    }
+
+    public List<City> getItems(int offset, int limit) {
+        Query<City> query = sessionFactory.getCurrentSession().createQuery(
+                "from City", City.class);
+        query.setFirstResult(offset);
+        query.setMaxResults(limit);
+        return query.list();
+    }
+
+    public int getCount() {
+        Query query = sessionFactory.getCurrentSession().createQuery(
+                "select count(*) from City");
+        return Math.toIntExact((Long) query.uniqueResult());
+    }
 }
--- src/main/java/org/staimov/dao/CountryDAO.java
+++ src/main/java/org/staimov/dao/CountryDAO.java
@@ -1,4 +1,20 @@
 package org.staimov.dao;
 
+import org.hibernate.SessionFactory;
+import org.hibernate.query.Query;
+import org.staimov.domain.Country;
+
+import java.util.List;
+
 public class CountryDAO {
+    private final SessionFactory sessionFactory;
+
+    public CountryDAO(SessionFactory sessionFactory) {
+        this.sessionFactory = sessionFactory;
+    }
+
+    public List<Country> getAll() {
+        return sessionFactory.getCurrentSession().createQuery(
+                "select c from Country c join fetch c.languages", Country.class).list();
+    }
 }
