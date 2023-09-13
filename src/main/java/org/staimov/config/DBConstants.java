ea5e2696b9a1a53102af86640f3f0aa9363dd316
staimov <staimov@mail.ru>
2023-09-13 12:48:36 +0500

    redis constants added

--- src/main/java/org/staimov/config/DBConstants.java
+++ src/main/java/org/staimov/config/DBConstants.java
@@ -9,4 +9,6 @@ public class DBConstants {
     public static final String DB_URL = "jdbc:p6spy:mysql://localhost:63306/world";
     public static final String CURRENT_SESSION_CONTEXT_CLASS = "thread";
     public static final String STATEMENT_BATCH_SIZE = "100";
+    public static final String REDIS_HOST = "localhost";
+    public static final int REDIS_PORT = 6379;
 }
