6c1775aedfc6dfdea1acea26ee89c457bb7e6e22
staimov <staimov@mail.ru>
2023-09-13 12:54:27 +0500

    FETCH_STEP added

--- src/main/java/org/staimov/config/DBConstants.java
+++ src/main/java/org/staimov/config/DBConstants.java
@@ -9,6 +9,7 @@ public class DBConstants {
     public static final String DB_URL = "jdbc:p6spy:mysql://localhost:63306/world";
     public static final String CURRENT_SESSION_CONTEXT_CLASS = "thread";
     public static final String STATEMENT_BATCH_SIZE = "100";
+    public static final int FETCH_STEP = 500;
     public static final String REDIS_HOST = "localhost";
     public static final int REDIS_PORT = 6379;
 }
