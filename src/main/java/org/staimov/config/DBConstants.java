64a70d8872e92466f5eab816413f1dbc8099a9b7
staimov <staimov@mail.ru>
2023-10-14 09:51:45 +0500

    p6spy removed

--- src/main/java/org/staimov/config/DBConstants.java
+++ src/main/java/org/staimov/config/DBConstants.java
@@ -5,8 +5,8 @@ public class DBConstants {
     public static final String DB_USER = "root";
     public static final String DB_PASS = "root";
     public static final String DB_HBM2DDL_AUTO = "validate";
-    public static final String DB_DRIVER = "com.p6spy.engine.spy.P6SpyDriver";
-    public static final String DB_URL = "jdbc:p6spy:mysql://localhost:63306/world";
+    public static final String DB_DRIVER = "com.mysql.jdbc.Driver";
+    public static final String DB_URL = "jdbc:mysql://localhost:63306/world";
     public static final String CURRENT_SESSION_CONTEXT_CLASS = "thread";
     public static final String STATEMENT_BATCH_SIZE = "100";
     public static final int FETCH_STEP = 500;
